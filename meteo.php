29f6868d5e379e1e9614efd8d9260e56b89c9df0
Florence Bouvier <florencebres1@gmail.com>
2019-08-30 10:39:27 +0200

    clean file

--- meteo.php
+++ meteo.php
@@ -1,24 +1,18 @@
 <!-- <?php
 //initialise une session
 
-//$curl = curl_init('http://api.openweathermap.org/data/2.5/weather?q=Lyon&units=metric&appid=5228063350a4e4c7d5a1c770159434a3');
-//curl_setopt_array($curl,[
-//      CURLOPT_CAINFO => __DIR__.DIRECTORY_SEPARATOR.'cert.cer',
-//      CURLOPT_RETURNTRANSFER => true,
-//      CURLOPT_TIMEOUT => 1
-//]);
+$curl = curl_init('http://api.openweathermap.org/data/2.5/weather?q=Lyon&units=metric&appid=5228063350a4e4c7d5a1c770159434a3');
+curl_setopt_array($curl,[
+      CURLOPT_CAINFO => __DIR__.DIRECTORY_SEPARATOR.'cert.cer',
+     CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_TIMEOUT => 1
+]);
 
 // pour executer curl
-//$data = curl_exec($curl);
-//pour récupérer l'erreur
-//if ($data === false){
-//      var_dump(curl_error($curl));
-//} else {
-//            $data = json_decode($data, true);
-//      echo 'il fait '.$data['main']['temp'].' C° à Lyon. ';
-
-//      }
-//}
-// pour fermer
-//curl_close($curl);
-?> -->
+$data = curl_exec($curl);
+//pour recuperer l'erreur
+if ($data === false ||curl_getinfo($curl,CURLINFO_HTTP_CODE) !== 200){
+     var_dump(curl_error($curl));
+} else {
+            $data = json_decode($data, true);
+     echo 'il fait '.$data['main']['temp'].' C° 
